09fa5b39831918ef9d2ee7044287d24244718d19
DIEGO ENMANUEL ZEPEDA RENDEROS <USSS017922@ugb.edu.sv>
2022-08-26 18:14:43 -0600

    Formulario

---
+++ Vertebrados.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_Educativa_Sobre_Animales
+{
+    public partial class Vertebrados : Form
+    {
+        public Vertebrados()
+        {
+            InitializeComponent();
+        }
+    }
+}
