165c3e71c8bd753c0caef1ec0fa8fa919dc3647c
Edwin <usss017222@ugb.edu.sv>
2022-08-27 14:38:03 -0600

    Pequeño ajuste

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -130,7 +130,7 @@ namespace App_Educativa_Sobre_Animales
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(140, 55);
             this.button3.TabIndex = 3;
-            this.button3.Text = "Domésticos";
+            this.button3.Text = "Domésti";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
